2fea278f76cfa7594da03b512a59fdc3dba7ec5f
Sinequanonh <leo.baecker@epitech.eu>
2017-03-09 01:11:00 +0100

    🔥 mp3 version of youtube-dl

---
+++ tubeme3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $1 == *"youtube"* ]]
+then
+    echo "Downloading from youtube"
+    youtube-dl -x --embed-thumbnail --add-metadata --metadata-from-title "%(artist)s - %(title)s" --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTu\
+nes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/'
+fi
+if [[ $1 == *"soundcloud"* ]]
+then
+    echo "Downloading from soundcloud"
+    youtube-dl -x --embed-thumbnail --add-metadata --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTune\
+s.localized/'
+fi
