aabce4074ddeac4ad2d84ff77fba4eff9c321a4c
Sinequanonh <leo.baecker@epitech.eu>
2017-03-09 01:47:12 +0100

    Issue regarding thumbnail embedding

--- tubeme3.sh
+++ tubeme3.sh
@@ -3,12 +3,12 @@
 if [[ $1 == *"youtube"* ]]
 then
     echo "Downloading from youtube"
-    youtube-dl -x --embed-thumbnail --add-metadata --metadata-from-title "%(artist)s - %(title)s" --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTu\
+    youtube-dl -x --embed-thumbnail --prefer-ffmpeg --add-metadata --metadata-from-title "%(artist)s - %(title)s" --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTu\
 nes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/'
 fi
 if [[ $1 == *"soundcloud"* ]]
 then
     echo "Downloading from soundcloud"
-    youtube-dl -x --embed-thumbnail --add-metadata --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTune\
+    youtube-dl -x --embed-thumbnail --prefer-ffmpeg --add-metadata --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" $1 --exec 'mv {} /Users/lbaecker/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTune\
 s.localized/'
 fi
